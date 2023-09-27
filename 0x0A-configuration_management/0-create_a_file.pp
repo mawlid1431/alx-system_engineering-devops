7331c58cc57df573afc182252f10c72b5fb20256
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-27 13:25:05 +0300

    modified the project that is clled 0x0A config

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #The code will create a file name  school inside the /tmp di
 file{`/tmp/school` ;
 ensure => file, 
-content => `I love Puppet`,
 mode  => `0744`,
 owner => `www-dat`,
 group =>  www.data`,
+content => `I love Puppet`,
 } 
 
